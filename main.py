018ac920a2531352bd25fbbec3462424f6acfe59
Wojciech Blachowski <wblachowsky@gmail.com>
2021-01-15 16:33:22 +0100

    Reverse tree building logic + add print method to the node

--- main.py
+++ main.py
@@ -11,25 +11,38 @@ class Node():
         self.synset = synset
         self.children = []
 
+    def __str__(self, level=0):
+        res = "\t" * level
+        res += self.synset._name
+        for child in self.children:
+            res += '\n' + child.__str__(level + 1)
+        return res
+
 
 def build_tree_from_synset_to_entity(synset):
-    head = Node(synset)
-    dfs(head)
-    return head
+    heads = dfs([Node(synset)])
+    print(heads[0])
+    return heads
 
 
-def dfs(head):
-    for hypernym in head.synset.hypernyms():
-        head.children.append(Node(hypernym))
-    for child in head.children:
-        dfs(child)
+def dfs(heads):
+    new_heads = []
+    for head in heads:
+        can_extend = False
+        for hypernym in head.synset.hypernyms():
+            can_extend = True
+            node = Node(hypernym)
+            node.children.append(head)
+            new_heads.extend(dfs([node]))
+        if not can_extend:
+            new_heads.append(head)
+    return new_heads
 
 
 def get_top_synstets(words, pos=wn.NOUN):
     synsets = [wn.synsets(word, pos) for word in words]
     dog = synsets[1][0]
-    head = build_tree_from_synset_to_entity(dog)
-    print(head.synset)
+    heads = build_tree_from_synset_to_entity(dog)
 
 
 get_top_synstets(['cat', 'dog'])
